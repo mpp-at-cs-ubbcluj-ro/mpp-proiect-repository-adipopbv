b57bac8206d2c9f90994dfbd6092db1f398233a9
Adrian Pop <adi.pop.bv@gmail.com>
2021-03-20 13:46:21 +0200

    Added base javafx login window

--- pr1-java/src/main/java/Main.java
+++ pr1-java/src/main/java/Main.java
@@ -1,3 +1,4 @@
+import clients.JavaFxClient;
 import domain.Game;
 import domain.Ticket;
 import domain.User;
@@ -21,26 +22,28 @@ public class Main {
             System.out.println("Cannot find db.config | " + e);
         }
 
-        UserRepository userRepository = new UserDbRepository(properties);
-        GameRepository gameRepository = new GameDbRepository(properties);
-        TicketRepository ticketRepository = new TicketDbRepository(properties);
-        userRepository.add(new User(1, "adipopbv", "logged-out"));
-        gameRepository.add(new Game(1, "Semifinals", "Rockets", "Celtics", 300, 200));
-        ticketRepository.add(new Ticket(1, gameRepository.getOne(1), "Pop Adrian"));
-        System.out.println("All users: " + userRepository);
-        System.out.println("All games: " + gameRepository);
-        System.out.println("All tickets: " + ticketRepository);
-        userRepository.modify(1, new User(1, "adipopbv", "logged-in"));
-        gameRepository.modify(1, new Game(1, "Finals", "Celtics", "Rockets", 450, 150));
-        ticketRepository.modify(1, new Ticket(1, gameRepository.getOne(1), "Adrian Pop"));
-        System.out.println("All users: " + userRepository);
-        System.out.println("All games: " + gameRepository);
-        System.out.println("All tickets: " + ticketRepository);
-        userRepository.remove(1);
-        gameRepository.remove(1);
-//        ticketRepository.remove(1);
-        System.out.println("All users: " + userRepository);
-        System.out.println("All games: " + gameRepository);
-        System.out.println("All tickets: " + ticketRepository);
+        JavaFxClient.main(args);
+
+//        UserRepository userRepository = new UserDbRepository(properties);
+//        GameRepository gameRepository = new GameDbRepository(properties);
+//        TicketRepository ticketRepository = new TicketDbRepository(properties);
+//        userRepository.add(new User(1, "adipopbv", "logged-out"));
+//        gameRepository.add(new Game(1, "Semifinals", "Rockets", "Celtics", 300, 200));
+//        ticketRepository.add(new Ticket(1, gameRepository.getOne(1), "Pop Adrian"));
+//        System.out.println("All users: " + userRepository);
+//        System.out.println("All games: " + gameRepository);
+//        System.out.println("All tickets: " + ticketRepository);
+//        userRepository.modify(1, new User(1, "adipopbv", "logged-in"));
+//        gameRepository.modify(1, new Game(1, "Finals", "Celtics", "Rockets", 450, 150));
+//        ticketRepository.modify(1, new Ticket(1, gameRepository.getOne(1), "Adrian Pop"));
+//        System.out.println("All users: " + userRepository);
+//        System.out.println("All games: " + gameRepository);
+//        System.out.println("All tickets: " + ticketRepository);
+//        userRepository.remove(1);
+//        gameRepository.remove(1);
+////        ticketRepository.remove(1);
+//        System.out.println("All users: " + userRepository);
+//        System.out.println("All games: " + gameRepository);
+//        System.out.println("All tickets: " + ticketRepository);
     }
 }
---
+++ pr1-java/src/main/java/clients/controllers/LogInController.java
@@ -0,0 +1,22 @@
+package clients.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class LogInController {
+    @FXML
+    public Button logInAcceptButton;
+    @FXML
+    public TextField logInUserIdField;
+    @FXML
+    public Button logInToSignUpButton;
+
+    public void logIn() {
+
+    }
+
+    public void toSignUp() {
+
+    }
+}
