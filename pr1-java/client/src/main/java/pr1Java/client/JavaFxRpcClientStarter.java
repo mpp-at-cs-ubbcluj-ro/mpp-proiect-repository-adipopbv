622381362176fda59cb82d7fd7bc0a2991693a9f
Adrian Pop <adi.pop.bv@gmail.com>
2021-04-13 10:17:56 +0300

    Minor fixes

--- pr1-java/client/src/main/java/pr1Java/client/JavaFxRpcClientStarter.java
+++ pr1-java/client/src/main/java/pr1Java/client/JavaFxRpcClientStarter.java
@@ -6,7 +6,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import pr1Java.client.clients.SignInClient;
-import pr1Java.networking.rpcProtocol.ServicesRpcProxy;
 import pr1Java.services.IServices;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
